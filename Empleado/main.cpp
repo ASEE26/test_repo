988906ac8e8265a2bea66b6b5edb6b98f9cd4a96
YenCa <90876846+YenCa@users.noreply.github.com>
2021-10-27 18:46:21 -0600

    Update main.cpp
    Se agrego un dato mas para imprimir en pantalla que es el No. Talento Humano

--- Empleado/main.cpp
+++ Empleado/main.cpp
@@ -6,12 +6,12 @@ using std::cout;
 
 int main(){
 	
-	Empleado empleado1("Pablo", "Mejia");
+	Empleado empleado1("Pablo", "Mejia", "0501199000450", "01470");
 	
 	cout<<"El nombre del empleado es: "<<empleado1.getPrimerNombre()<<endl;
 	cout<<"El apellido del empleado es: "<<empleado1.getPrimerApellido()<<endl;
 	cout<<"La identidad del empleado es: "<<empleado1.getIdentidad()<<endl;
-	cout<<"La cantidad de empleados es: "<<empleado1.getNumEmpleados()<<endl;	
-
+	cout<<"La cantidad de empleados es: "<<empleado1.getNumEmpleados()<<endl;
+	cout<<"El No. Talento Humano del empleados es: "<<empleado1.gettalentoHumano()<<endl;
 	return 0;
 }

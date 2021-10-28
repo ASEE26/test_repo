23392cfc84a5ae2d9982d17cac2177136842e3ad
YenCa <90876846+YenCa@users.noreply.github.com>
2021-10-27 18:23:27 -0600

    Update Empleado.hpp
    Agregue un miembro privado iD tipo char.

--- Empleado/Empleado.hpp
+++ Empleado/Empleado.hpp
@@ -2,16 +2,17 @@ class Empleado
 {
 
 public:
-    Empleado(const char *const, const char *const );
+    Empleado(const char *const, const char *const, const char *const);
 
     const char *getPrimerNombre() const;
     const char *getPrimerApellido() const;
-
+    const char *getIdentidad() const;
     static const int getNumEmpleados();
     void contar();
 
 private:
     char *primerNombre;
     char *primerApellido;
+    char *iD;
     static int numEmpleados;
 };
